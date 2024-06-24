95a4c47fd75547f0ad5c0be2d0636f42ec17b592
FProject18 <139376988+FaisalBAkbar544@users.noreply.github.com>
2024-06-24 23:44:11 +0800

    Bismillah BackEndDeveloper 3 digits

---
+++ src/t4/hallofaisal/T4HalloFaisal.java
@@ -0,0 +1,13 @@
+package t4.hallofaisal;
+
+/**
+ *
+ * @author Muhammad Faisal Akbar
+ */
+public class T4HalloFaisal {
+
+    public static void main(String[] args) {
+        System.out.println("Muhammad Faisal Akbar");
+    }
+
+}
